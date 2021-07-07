cd8c035d5088a959f0e9781e56d7a64e306ece5d
loonaticx <l.oony@aol.com>
2021-07-07 02:06:10 -0500

    Improve clothing script

---
+++ scripts/clothingpreview/README.md
@@ -0,0 +1,30 @@
+# Clothing Render / Previewer
+
+## Usage
+You can load in shirt, sleeve, and short textures (skirt wip) and take transparent render screenshots
+to use for graphic design/promotional purposes.
+
+If you actually use this script, it might yell at you for missing textures. They don't really matter much.
+
+## Controls
+
+s = Take screenshot
+o = toggle oobe/free camera
+r = reload loaded textures
+1 = toggle shirt
+2 = toggle bottoms
+mouse wheel up = zoom in
+mouse wheel down = zoom out
+mouse3 = reset zoom
+left arrow = rotate negative heading
+right arrow = rotate positive heading
+up arrow = rotate positive pitch
+down arrow = rotate negative pitch
+
+## Todo
+
+Torso only frame (better),
+Bottom only frame too (better),
+m and f body types (specifically skirts),
+different body types (fat, skinny)
+Add onscreen text that displays the offset (camers zoom, clothing rotation, etc.) <-- will be hidden in screenshots
--- scripts/clothingpreview/preview.py
+++ scripts/clothingpreview/preview.py
@@ -1,10 +1,10 @@
 from direct.showbase.ShowBase import ShowBase
 from pathlib import Path
-from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 from panda3d.core import Filename
 from panda3d.core import GraphicsOutput
 from direct.gui.DirectGui import *
+import sys, os
 
 # We need to import the tkinter library to
 # disable the tk window that pops up.
@@ -14,25 +14,34 @@ root = tk.Tk()
 root.withdraw()
 
 # Force high quality for our render
-from pandac.PandaModules import loadPrcFileData
+from panda3d.core import loadPrcFileData
 loadPrcFileData('', 'default-antialias-enable 1')
 loadPrcFileData('', 'framebuffer-multisample 1')
 
 """
 Controls:
 s = Take screenshot
-o = oobe (buggy rn sadge)
+o = toggle oobe/free camera
 r = reload loaded textures
+1 = toggle shirt
+2 = toggle bottoms
+mouse wheel up = zoom in
+mouse wheel down = zoom out
+mouse3 = reset zoom
+left arrow = rotate negative heading
+right arrow = rotate positive heading
+up arrow = rotate positive pitch
+down arrow = rotate negative pitch
+
 """
 
 """
 Todo:
-Torso only frame,
-Bottom only frame too,
-m and f body types,
-different body types
-maybe show back of outfit on the right side
-Add onscreen text that displays the offset (camers zoom, clothing rotation, etc.)
+Torso only frame (better),
+Bottom only frame too (better),
+m and f body types (specifically skirts),
+different body types (fat, skinny)
+Add onscreen text that displays the offset (camers zoom, clothing rotation, etc.) <-- will be hidden in screenshots
 """
 
 
@@ -40,12 +49,17 @@ class previewClothing(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
-        self.fileName = "output.png" # Output file namee
+        self.fileName = "output" # Output file name
+        self.fileFormat = ".png"
         self.topTex = None
         self.sleeveTex = None
         self.bottomTex = None
         self.loadedTextures = [None, None, None]
+        self.shirtVisible = True
+        self.bottomsVisible = True
         self.defaultCamPos = base.cam.getPos()
+        base.camera.hide()
+        self.i = 1
         
         # Just in case we have these enabled in the config...
         base.setFrameRateMeter(False)
@@ -64,7 +78,7 @@ class previewClothing(ShowBase):
        
         self.accept('s', self.aspect2d.hide) # Hacky b/c hiding and showing in same method no work
         self.accept('s-up', self.saveScreenshot)
-        self.accept('o', self.freeCam)
+        self.accept('o', base.oobe)
         self.accept('r', self.reloadTextures)
         self.accept('wheel_up', self.incrementCam)
         self.accept('wheel_down', self.decrementCam)
@@ -73,10 +87,15 @@ class previewClothing(ShowBase):
         self.accept('arrow_left-repeat', self.decrementH)
         self.accept('arrow_right', self.incrementH)
         self.accept('arrow_right-repeat', self.incrementH)
+        self.accept('arrow_up', self.incrementP)
+        self.accept('arrow_up-repeat', self.incrementP)
+        self.accept('arrow_down', self.decrementP)
+        self.accept('arrow_down-repeat', self.decrementP)
+        self.accept('1', self.toggleShirt)
+        self.accept('2', self.toggleBottoms)
         #self.accept('b', self.torso.showTightBounds)
 
        
-
     """
     ("tt_a_chr_dgX_shorts_torso_1000.egg")
     X = s, m, l
@@ -85,9 +104,7 @@ class previewClothing(ShowBase):
         torsoModel = loader.loadModel("tt_a_chr_dgm_shorts_torso_1000.egg") # can rename this later
         self.torso = torsoModel.getChild(0)
         self.torso.reparentTo(render)
-        
-        
-
+               
         #torsoModel.hide()
         for node in self.torso.getChildren():
             if (node.getName() != 'torso-top')\
@@ -95,11 +112,9 @@ class previewClothing(ShowBase):
             and (node.getName() != 'sleeves'):
                 node.stash()
 
-        self.torso.setPosHprScale(0.00, 4.69, -0.235, 190.30, 0.00, 0.00, 1.00, 1.00, 1.00)
+        self.torso.setPosHprScale(0.00, 4.69, -0.235, 190, 0.00, 0.00, 1.00, 1.00, 1.00)
         self.torso.setTwoSided(True)
-
-        
-        
+    
     def loadGUI(self):
         # Todo: figure out how to reposition buttons when window changes size
         #guiFrame = DirectFrame(frameColor=(0, 0, 0, 1),
@@ -111,29 +126,64 @@ class previewClothing(ShowBase):
                  scale=0.05, pos=(-1.6, 0, -0.5), parent=base.aspect2d, command=self.openSleeves)
         self.shortsButton = DirectButton(text=("Change Bottoms"),
                  scale=0.05, pos=(-1.6, 0, -0.6), parent=base.aspect2d, command=self.openBottom)
-
         
     def saveScreenshot(self):
-        base.win.saveScreenshot(Filename(self.fileName))
-        self.aspect2d.show()
+        # intent: Image number would increment if the file already exists just so it doesn't overwrite
+        self.newfileName = self.fileName
+        if not (os.path.isfile(self.newfileName)): # wip
+            self.newfileName = self.fileName+str(self.i)
+            self.i +=1
         
-    def freeCam(self):
-        base.oobe()
+        filename = self.newfileName +self.fileFormat
+        base.win.saveScreenshot(Filename(filename))
+        self.aspect2d.show()
+        print("Screenshot saved! {}".format(filename))
+        
 
     def reloadTextures(self):
         for tex in self.loadedTextures:
             if tex: tex.reload()
     
+    # temporary until i have a better way to do this lol    
+    def toggleShirt(self):
+        if (self.shirtVisible):
+            self.torso.find('**/torso-top').hide()
+            self.torso.find('**/sleeves').hide()
+            self.shirtVisible = False
+        else:
+            self.torso.find('**/torso-top').show()
+            self.torso.find('**/sleeves').show()
+            self.shirtVisible = True
+    
+    def toggleBottoms(self):
+        if (self.bottomsVisible):
+            self.torso.find('**/torso-bot').hide()
+            self.bottomsVisible = False
+        else:
+            self.torso.find('**/torso-bot').show()
+            self.bottomsVisible = True
+    
+    
+    # Rotate clothing
+    
     def incrementH(self):
-        self.rotateClothingH(1)
+        self.rotateClothingH(5)
     
     def decrementH(self):
-        self.rotateClothingH(-1)
+        self.rotateClothingH(-5)
     
     def rotateClothingH(self, value):
         self.torso.setH(self.torso.getH() + value)
+        
+    def incrementP(self):
+        self.rotateClothingP(5)
     
+    def decrementP(self):
+        self.rotateClothingP(-5)
     
+    def rotateClothingP(self, value):
+        self.torso.setP(self.torso.getP() + value)
+        
     # Camera Modifiers
     def defaultCam(self):
         base.cam.setPos(self.defaultCamPos)
@@ -184,7 +234,6 @@ class previewClothing(ShowBase):
             self.loadTopTexture(filename)
         except:
             print(str(filename) + " could not be loaded!")
-            messagebox.showerror("Error", "Unable to load texture: " + str(filename))
 
     def openSleeves(self):
         filename = self.browseForImage()
@@ -194,7 +243,6 @@ class previewClothing(ShowBase):
             self.loadSleeveTexture(filename)
         except:
             print(str(filename) + " could not be loaded!")
-            messagebox.showerror("Error", "Unable to load texture: " + str(filename))
 
     def openBottom(self):
         filename = self.browseForImage()
@@ -204,7 +252,6 @@ class previewClothing(ShowBase):
             self.loadBottomTexture(filename)
         except:
             print(str(filename) + " could not be loaded!")
-            messagebox.showerror("Error", "Unable to load texture: " + str(filename))
         
 
 app = previewClothing()
