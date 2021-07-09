a0d0ea8cc17a5b31dcf43662be7698b623cf7081
loonaticx <l.oony@aol.com>
2021-07-09 01:36:32 -0500

    Tweak texmem

--- scripts/texmem/README.md
+++ scripts/texmem/README.md
@@ -4,3 +4,5 @@ I'll have to write about this later.
 ``texmem_explore`` was my experimental script & includes a bunch of junk that's currently not related to texmem. I will clean it up later.
 
 todo: add cool pictures and explanations and stuff
+
+
--- scripts/texmem/texmem_explore.py
+++ scripts/texmem/texmem_explore.py
@@ -8,6 +8,8 @@ from direct.gui.DirectGui import *
 import sys, os
 
 from direct.showutil.TexMemWatcher import TexMemWatcher
+from panda3d.core import loadPrcFileData
+loadPrcFileData('', 'model-path $DEV_P3D')
 
 # We need to import the tkinter library to
 # disable the tk window that pops up.
@@ -15,10 +17,7 @@ from direct.showutil.TexMemWatcher import TexMemWatcher
 import tkinter as tk
 root = tk.Tk()
 root.withdraw()
-
-from panda3d.core import loadPrcFileData
-loadPrcFileData('', 'show-buffers 1')
-
+# todo: make tex mem watcher spawn in a larger window so it can crash less
 # https://docs.panda3d.org/1.10/python/reference/direct.showutil.TexMemWatcher#module-direct.showutil.TexMemWatcher
 
 class driver(ShowBase):
@@ -26,28 +25,29 @@ class driver(ShowBase):
         ShowBase.__init__(self)
         self.base = ShowBase
         self.model = None
+        self.defaultCamPos = base.cam.getPos()
         base.setSceneGraphAnalyzerMeter(True)
         #self.base.toggleTexMem(self)
-        # Sometimes the texture memory viewer will crash on certain files
+        # WARNING: Sometimes the texture memory viewer will crash on certain files
         self.TexMemWatcher = TexMemWatcher()
         self.accept('1', base.oobe)
         self.accept('2', base.oobeCull)
-        base.bufferViewer.toggleEnable()
-        
+        self.accept('3', base.showCameraFrustum)
+        #self.accept('4', base.hideCameraFrustum)
+
         self.loadGUI()
-        # probably want to re-arrange this sometime
-        self.accept('q', base.showCameraFrustum)
-        self.accept('w', render.analyze)
-        self.accept('e', self.resetCam)
-        self.accept('r', self.clearScene)
+
+        self.accept('a', render.analyze)
+        self.accept('r', self.resetCam)
+        self.accept('c', self.clearScene)
         self.accept('t', base.toggleTexture)
-        self.accept('y', base.toggleBackface)
-        self.accept('u', base.printEnvDebugInfo)
+        self.accept('b', base.toggleBackface)
+
+
         self.accept('i', self.sphere)
         self.accept('o', self.cube)
 
-        
-        #render.analyze()
+
 
     def loadGUI(self):
         # Todo: figure out how to reposition buttons when window changes size
@@ -60,7 +60,7 @@ class driver(ShowBase):
     def browseModel(self):
     # does direct.showbase.Loader.loadModel support any of the normal model types like obj/fbx?
         path = Path(askopenfilename(filetypes = (
-            ("Image Files", "*.egg;*.bam"),
+            ("Panda3D Model Files", "*.egg;*.bam"),
             ("EGG", "*.egg"),
             ("BAM", "*.bam"))))
 
@@ -83,27 +83,17 @@ class driver(ShowBase):
         print("Loaded {}".format(file))
 
     def resetCam(self):
-        base.oobe()
-        base.cam.setPosHpr(0, 0, 0, 0, 0, 0)
-        base.oobe()
+        base.cam.setPos(self.defaultCamPos)
 
     def clearScene(self):
         if self.model is not None:
             self.model.removeNode()
             self.model = None
-    
-    def graphicsLimit(self):
-        self.TexMemWatcher.setLimit()
-    
-    def findLargestHole(self):
-        print(self.TexMemWatcher.findLargestHole())
-    
-    def pipe(self):
-        print(base.pipeList())
-    
+
+
     def sphere(self):
         base.saveSphereMap(size=1024)
-    
+
     def cube(self):
         base.saveCubeMap(size=1024)
 
