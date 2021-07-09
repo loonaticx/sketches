ca12265a011cf9239e05e05281a6d72869d36102
loonaticx <l.oony@aol.com>
2021-07-09 01:31:41 -0500

    Move png conversion script to the scripts folder

--- pngconv/pngconv.py
+++
@@ -1,6 +0,0 @@
-# requires alphacombiner library
-
-import alphacombiner
-
-img = alphacombiner.ImageConverter('sample/')
-img.convert_texture(['boarding_gui_palette_4alla_1.jpg', 'boarding_gui_palette_4alla_1_a.rgb'])
