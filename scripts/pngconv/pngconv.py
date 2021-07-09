0883260d493cb9d45f67177a95767167c789c627
loonaticx <l.oony@aol.com>
2021-07-09 01:34:43 -0500

    Move png conversion script to the scripts folder part 2

---
+++ scripts/pngconv/pngconv.py
@@ -0,0 +1,6 @@
+# requires alphacombiner library
+
+import alphacombiner
+
+img = alphacombiner.ImageConverter('sample/')
+img.convert_texture(['gamegui_palette_2tlla_1', 'gamegui_palette_2tlla_1_a.rgb'])
