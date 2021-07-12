3ac2e1d94ce4a0c9233efcef0b09538ffdca2cc5
loonaticx <l.oony@aol.com>
2021-07-12 06:34:25 -0500

    Add color modifier demo

---
+++ scripts/colordemo/color_explore.py
@@ -0,0 +1,130 @@
+from direct.tkwidgets.MemoryExplorer import MemoryExplorer
+from direct.showbase.ShowBase import ShowBase
+from pathlib import Path
+from tkinter.filedialog import askopenfilename
+from direct.gui.DirectGui import *
+import sys, os
+
+from direct.showutil.TexMemWatcher import TexMemWatcher
+from panda3d.core import loadPrcFileData
+loadPrcFileData('', 'model-path $DEV_P3D')
+
+# We need to import the tkinter library to
+# disable the tk window that pops up.
+# We use tk for the file path selector.
+import tkinter as tk
+root = tk.Tk()
+root.withdraw()
+
+class driver(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.base = ShowBase
+        self.model = None
+        self.defaultCamPos = base.cam.getPos()
+
+        self.colorScale = [0] # default we will use setColor
+
+        self.colorR = 1
+        self.colorG = 1
+        self.colorB = 1
+        self.colorA = 1
+        base.setSceneGraphAnalyzerMeter(False)
+
+        self.loadGUI()
+
+        self.accept('c', self.clearScene)
+        self.accept('t', self.toggleTexture)
+
+
+    def loadGUI(self):
+        self.topButton = DirectButton(text=("Load model"),
+                 scale=0.05, pos=(0, 0, -0.90), parent=base.aspect2d, command=self.loadFile)
+
+        buttonSpacing = 0.1
+        buttonbase_xcoord = -1.4
+        buttonbase_ycoord = -0.20
+
+
+        self.buttonColorR = DirectSlider(value=self.colorR,
+                                pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 4),
+                                range=(0, 1),
+                                frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                command=self.changeColorValue)
+
+        self.buttonColorB = DirectSlider(value=self.colorG,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 5),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+        self.buttonColorG = DirectSlider(value=self.colorB,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 6),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+        self.buttonColorA = DirectSlider(value=self.colorA,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 7),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+        # todo maybe make this a radio button instead
+        self.buttonColorMode = [
+        DirectRadioButton(text='setColorScale', variable=self.colorScale, value=[1],
+                        scale=0.05, pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 3), command=self.changeColorValue),
+        DirectRadioButton(text='setColor', variable=self.colorScale, value=[0],
+                        scale=0.05, pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 2), command=self.changeColorValue)
+        ]
+        for button in self.buttonColorMode:
+            button.setOthers(self.buttonColorMode)
+
+    def changeColorValue(self):
+        self.colorR = self.buttonColorR['value']
+        self.colorG = self.buttonColorB['value']
+        self.colorB = self.buttonColorG['value']
+        self.colorA = self.buttonColorA['value']
+        if self.colorScale[0]:
+            for node in render.getChildren():
+                node.clearColor()
+                node.setColorScale(self.colorR, self.colorB, self.colorG, self.colorA)
+        else:
+            for node in render.getChildren():
+                node.clearColorScale()
+                node.setColor(self.colorR, self.colorB, self.colorG, self.colorA)
+
+    def browseModel(self):
+    # does direct.showbase.Loader.loadModel support any of the normal model types like obj/fbx?
+        path = Path(askopenfilename(filetypes = (
+            ("Panda3D Model Files", "*.egg;*.bam"),
+            ("EGG", "*.egg"),
+            ("BAM", "*.bam"))))
+
+        return path
+
+    def loadFile(self):
+        filename = self.browseModel()
+        if str(filename) == ".":
+            return
+        try:
+            self.loadModel(filename)
+        except:
+            print(str(filename) + " could not be loaded!")
+
+    def loadModel(self, file: str):
+        self.clearScene()
+        self.model = loader.loadModel(file)
+        self.model.reparentTo(render)
+        print("========================================")
+        print("Loaded {}".format(file))
+
+
+    def clearScene(self):
+        if self.model is not None:
+            self.model.removeNode()
+            self.model = None
+
+
+app = driver()
+app.run()
---
+++ scripts/colordemo/demo.py
@@ -0,0 +1,166 @@
+from direct.showbase.ShowBase import ShowBase
+from pathlib import Path
+from panda3d.core import NodePath
+from direct.gui.DirectGui import *
+import sys, os
+
+from panda3d.core import loadPrcFileData
+loadPrcFileData('', 'model-path $DEV_P3D')
+
+# Demo
+# https://docs.panda3d.org/1.10/python/programming/render-attributes/tinting-and-recoloring
+
+# setColorScale = This color will be modulated (multiplied) <-- for tinting
+
+# setColor = If the model already had vertex colors, they will dissapear - method is replacing those colors with a new one
+# The reason why the 4th (alpha) slider doesn't do anything right now is because it is using RGB color mode & not RGBA
+
+# I can probably make this way better if I copy what I did for the explore script but w/e
+
+class demo(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        base.cam.setPos(0, -4, 0)
+        self.np = NodePath()
+        self.base = ShowBase
+        self.defaultH = 35
+        self.currentH = self.defaultH
+        self.defaultP = 360
+        self.currentP = self.defaultP
+        self.colorR = 0.5
+        self.colorG = 0.5
+        self.colorB = 0.5
+        self.colorA = 1
+        base.setSceneGraphAnalyzerMeter(False)
+        base.setFrameRateMeter(False)
+        self.loadDemo()
+        base.disableMouse()
+        self.loadGUI()
+
+        self.accept('1', self.toggleTexture)
+        self.accept('2', self.clearColor)
+        self.accept('3', self.clearColorScale)
+
+
+        self.accept('arrow_left', self.rotateH, [-5])
+        self.accept('arrow_left-repeat', self.rotateH, [-5])
+        self.accept('arrow_right', self.rotateH, [5])
+        self.accept('arrow_right-repeat', self.rotateH, [5])
+        self.accept('arrow_up', self.rotateP, [5])
+        self.accept('arrow_up-repeat', self.rotateP, [5])
+        self.accept('arrow_down', self.rotateP, [-5])
+        self.accept('arrow_down-repeat', self.rotateP, [-5])
+
+
+
+    """
+    cube.egg = Default primitive cube, no vertex color applied
+    cube2.egg = Default primitive cube, but 5 (out of 6) faces have been applied vertex color.
+    Two faces may look identical in color, one has no vertex painting applied & one has white vertex painting applied. Also double-sided.
+    No material is set on either of these.
+    """
+
+    def loadDemo(self):
+        cube1 = render.attachNewNode('cube1')
+        cube2 = render.attachNewNode('cube2')
+        cube3 = render.attachNewNode('cube3')
+        cube4 = render.attachNewNode('cube4')
+        self.cubeModel = loader.loadModel("cube.egg")
+        self.cubeModel.reparentTo(cube1)
+        self.cubeModel.setTexture(loader.loadTexture("tex.png"), 1)
+        self.cubeModel.setPosHpr(-1.00, 1.45, 0.65, self.currentH, self.currentP, 0)
+        self.cubeModel2 = self.cubeModel.copyTo(cube2)
+        self.cubeModel2.setPosHpr(1.00, 1.45, 0.65, self.currentH, self.currentP, 0)
+
+
+        self.cubeModel3 = loader.loadModel("cube2.egg")
+        self.cubeModel3.reparentTo(cube3)
+        self.cubeModel3.setTexture(loader.loadTexture("tex.png"), 1)
+        self.cubeModel3.setPosHpr(-1.00, 1.45, -0.65, self.currentH, self.currentP, 0)
+        self.cubeModel4 = self.cubeModel3.copyTo(cube4)
+        self.cubeModel4.setPosHpr(1.00, 1.45, -0.65, self.currentH, self.currentP, 0)
+
+
+
+    def loadGUI(self):
+        buttonSpacing = 0.1
+        buttonbase_xcoord = 0
+        buttonbase_ycoord = -0.25
+
+
+        self.buttonColorR = DirectSlider(value=self.colorR,
+                                pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 4),
+                                range=(0, 1),
+                                frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                command=self.changeColorValue)
+
+        self.buttonColorB = DirectSlider(value=self.colorG,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 5),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+        self.buttonColorG = DirectSlider(value=self.colorB,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 6),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+        self.buttonColorA = DirectSlider(value=self.colorA,
+                                  pos=(buttonbase_xcoord + 0.1, 0.0, buttonbase_ycoord - buttonSpacing * 7),
+                                  range=(0, 1),
+                                  frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                  command=self.changeColorValue)
+
+    def clearColorScale(self):
+        self.cubeModel.clearColorScale()
+        self.cubeModel3.clearColorScale()
+        # Intentionally not resetting the values on color variables
+
+    def clearColor(self):
+        self.cubeModel2.clearColor()
+        self.cubeModel4.clearColor()
+
+    def changeColorValue(self):
+        self.colorR = self.buttonColorR['value']
+        self.colorG = self.buttonColorB['value']
+        self.colorB = self.buttonColorG['value']
+        self.colorA = self.buttonColorA['value']
+        self.cubeModel.setColorScale(self.colorR, self.colorB, self.colorG, self.colorA)
+        self.cubeModel2.setColor(self.colorR, self.colorB, self.colorG, self.colorA)
+        self.cubeModel3.setColorScale(self.colorR, self.colorB, self.colorG, self.colorA)
+        self.cubeModel4.setColor(self.colorR, self.colorB, self.colorG, self.colorA)
+
+
+    # lazy, i know.
+
+    def rotateH(self, value):
+        self.currentH = self.currentH + value
+        self.cubeModel.setH(self.currentH)
+        self.cubeModel2.setH(self.currentH)
+        self.cubeModel3.setH(self.currentH)
+        self.cubeModel4.setH(self.currentH)
+
+    def rotateP(self, value):
+        self.currentP = self.currentP + value
+        self.cubeModel.setP(self.currentP)
+        self.cubeModel2.setP(self.currentP)
+        self.cubeModel3.setP(self.currentP)
+        self.cubeModel4.setP(self.currentP)
+
+    def defaultRotation(self):
+        self.currentH = self.defaultH
+        self.cubeModel.setH(self.currentH)
+        self.cubeModel2.setH(self.currentH)
+        self.cubeModel3.setH(self.currentH)
+        self.cubeModel4.setH(self.currentH)
+        self.currentP = self.defaultP
+        self.cubeModel.setP(self.currentP)
+        self.cubeModel2.setP(self.currentP)
+        self.cubeModel3.setP(self.currentP)
+        self.cubeModel4.setP(self.currentP)
+
+
+
+app = demo()
+app.run()
