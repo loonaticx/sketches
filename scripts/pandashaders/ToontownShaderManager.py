13f939f6ac039f395e3d2fd50886e789e72e7e2e
loonaticx <l.oony@aol.com>
2021-07-13 05:24:14 -0500

    work in pogress shader demo...

---
+++ scripts/pandashaders/ToontownShaderManager.py
@@ -0,0 +1,335 @@
+from direct.filter.CommonFilters import CommonFilters
+from panda3d.core import WindowProperties
+from panda3d.core import NodePath
+from panda3d.core import PGTop
+from panda3d.core import MouseWatcher
+from direct.gui.DirectFrame import DirectFrame
+from direct.gui.DirectSlider import DirectSlider
+from direct.gui.DirectButton import DirectButton
+from direct.gui.OnscreenText import OnscreenText
+
+"""
+  ToontownShaderManager
+  Demonstrating the use of Panda3D's built-in shaders.
+  Author: Loonatic
+  Date: 4/30/2021
+  https://gist.github.com/loonaticx/7c124790371cf3c5ac0b66c9da7c3765
+"""
+
+# https://docs.panda3d.org/1.10/python/_modules/direct/filter/CommonFilters
+# is it possible to render a new window & place the gui there instead of the model viewport window?
+# alternatively just have hotkeys/something to toggle certain gui since screen real estate is p tight
+
+class ToontownShaderManager(DirectFrame):
+
+    def __init__(self, parent):
+        self._parent = parent
+        DirectFrame.__init__(self, parent=self._parent, relief=None, pos=(0.0, 0.0, 0.0), scale=(1.0, 1.0, 1.0))
+        self.filter = CommonFilters(base.win, base.cam)
+
+        # Ambient Occlusion
+        self.samples = 0
+        self.radius = 0.0
+        self.amount = 0.0
+        self.strength = 0.0
+
+        # Blur/Sharpen
+        self.blur = 1.0 # this is normal value, 0.0 blurs it
+
+        # Cartoon Ink
+        self.cartoonSep = 0.0
+        self.cartoonR = 0.0
+        self.cartoonB = 0.0
+        self.cartoonG = 0.0
+
+        # todo: Add bloom
+
+        # Boolean Filters
+        self.HDREnabled = False
+        self.invertedEnabled = False
+        self.sRGBEnabled = False
+        self.halfPixelShiftEnabled = False
+        self.viewGlowEnabled = False
+
+        # Other Filters
+        self.exposure = 0.0
+
+        self.SAMPLES_MAX = 128
+        self.RAD_MAX = 1.0
+        self.AMT_MAX = 64.0
+        self.STR_MAX = 0.01
+        self.INCREMENT = 1
+        self.numSamples = None
+        self.numRadius = None
+
+        self.circleModel = loader.loadModel('phase_3/models/gui/tt_m_gui_mat_nameShop')
+        self.barTexture = loader.loadTexture('phase_3/maps/slider.png')
+        self.loadGUI()
+        self.newWindow()
+
+
+    def loadGUI(self):
+        self.textRowHeight = 0.2
+        self.buttonbase_xcoord = 1.4
+        self.buttonbase_ycoord = 0.45
+        #self.loadAmbientOcclusionGUI()
+        #self.loadBlurGUI()
+        #self.loadExposureGUI()
+        #self.loadCartoonInkGUI()
+        #self.loadHotkeys()
+
+    def loadHotkeys(self):
+        # for now instead of gui buttons i'ma just put the bool filters as hotkeys
+        self.accept('4', self.__toggleHDR)
+        self.accept('5', self.__toggleInverted)
+        self.accept('6', self.__toggleSRGB)
+        self.accept('7', self.__toggleHalfPixelShift)
+        self.accept('8', self.__toggleViewGlow)
+
+
+    def loadAmbientOcclusionGUI(self):
+        self.numSamples = DirectSlider(parent=self, value=self.samples,
+                                              pos=(self.buttonbase_xcoord + 0.1, 0.0, self.buttonbase_ycoord - self.textRowHeight * 3.5),
+                                              thumb_relief=None, range=(0, 32), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeAOValue)
+                                              #command=self.__changeAOValue(self.samples, self.radius, self.amount, self.strength))
+        self.numSamples.setScale(0.5)
+        self.numSamples.setTransparency(True)
+        self.numSamplesText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 3.2), scale=0.05, text="AO Sample Count = {}".format(self.samples), style=5, mayChange=True)
+
+
+        self.numRadius = DirectSlider(parent=self, value=self.radius,
+                                              pos=(self.buttonbase_xcoord + 0.1, 0.0, (self.buttonbase_ycoord - self.textRowHeight * 4.5)),
+                                              thumb_relief=None, range=(0, 1), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeAOValue)
+
+        self.numRadius.setScale(0.5)
+        self.numRadius.setTransparency(True)
+        self.numRadiusText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 4.2), scale=0.05, text="AO Radius = {}".format(str(self.radius)), style=5, mayChange=True)
+
+
+
+        self.numAmount = DirectSlider(parent=self, value=self.amount,
+                                              pos=(self.buttonbase_xcoord + 0.1, 0.0, (self.buttonbase_ycoord - self.textRowHeight * 5.5)),
+                                              thumb_relief=None, range=(0, 64), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeAOValue)
+
+        self.numAmount.setScale(0.5)
+        self.numAmount.setTransparency(True)
+        self.numAmountText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 5.2), scale=0.05, text="AO Amount = {}".format(self.amount), style=5, mayChange=True)
+
+
+        self.numStrength = DirectSlider(parent=self, value=self.strength,
+                                              pos=(self.buttonbase_xcoord + 0.1, 0.0, (self.buttonbase_ycoord - self.textRowHeight * 6.5)),
+                                              thumb_relief=None, range=(0, 0.1), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeAOValue)
+
+        self.numStrength.setScale(0.5)
+        self.numStrength.setTransparency(True)
+        self.numStrengthText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 6.2), scale=0.05, text="AO Strength = {}".format(self.strength), style=5, mayChange=True)
+
+    def loadCartoonInkGUI(self):
+        self.cSep = DirectSlider(parent=self, value=self.cartoonSep,
+                                              pos=(-self.buttonbase_xcoord + 0.1, 0.0, self.buttonbase_ycoord - self.textRowHeight * 2.5),
+                                              thumb_relief=None, range=(0, 32), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeCartoon)
+                                              #command=self.__changeAOValue(self.samples, self.radius, self.amount, self.strength))
+        self.cSep.reparentTo(self.window2render2d)
+        self.cSep.setScale(0.5)
+        self.cSep.setTransparency(True)
+
+        self.cRed = DirectSlider(parent=self, value=self.cartoonR,
+                                              pos=(-self.buttonbase_xcoord + 0.1, 0.0, self.buttonbase_ycoord - self.textRowHeight * 3.5),
+                                              thumb_relief=None, range=(0, 1), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeCartoon)
+                                              #command=self.__changeAOValue(self.samples, self.radius, self.amount, self.strength))
+        self.cRed.setScale(0.5)
+        self.cRed.setTransparency(True)
+
+        self.cBlue = DirectSlider(parent=self, value=self.cartoonB,
+                                              pos=(-self.buttonbase_xcoord + 0.1, 0.0, self.buttonbase_ycoord - self.textRowHeight * 4.5),
+                                              thumb_relief=None, range=(0, 1), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeCartoon)
+                                              #command=self.__changeAOValue(self.samples, self.radius, self.amount, self.strength))
+        self.cBlue.setScale(0.5)
+        self.cBlue.setTransparency(True)
+
+        self.cGreen = DirectSlider(parent=self, value=self.cartoonG,
+                                              pos=(-self.buttonbase_xcoord + 0.1, 0.0, self.buttonbase_ycoord - self.textRowHeight * 5.5),
+                                              thumb_relief=None, range=(0, 1), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeCartoon)
+                                              #command=self.__changeAOValue(self.samples, self.radius, self.amount, self.strength))
+        self.cGreen.setScale(0.5)
+        self.cGreen.setTransparency(True)
+
+    def loadBlurGUI(self):
+        self.numBlur = DirectSlider(parent=self.window2aspect2d, value=self.blur, pos=(0.0, 0.0, 0.0),
+                                              #pos=(self.buttonbase_xcoord + 0.1, 0.0, (self.buttonbase_ycoord - self.textRowHeight * 2.5)),
+                                              thumb_relief=None, range=(-10, 10), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeBlur)
+        self.numBlur.setScale(0.5)
+        self.numBlur.setTransparency(True)
+        self.numBlurText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 2.2), scale=0.05, text="Blur Amount = {}".format(self.blur), style=5, mayChange=True)
+
+
+    def loadExposureGUI(self):
+        self.numExposure = DirectSlider(parent=self, value=self.exposure,
+                                              pos=(self.buttonbase_xcoord + 0.1, 0.0, (self.buttonbase_ycoord - self.textRowHeight * 2.5)),
+                                              thumb_relief=None, range=(0, 5), #self.SAMPLES_MAX
+                                              thumb_geom=self.circleModel.find('**/tt_t_gui_mat_namePanelCircle'),
+                                              frameTexture=self.barTexture, frameSize=(-0.5, 0.5, -0.08, 0.08),
+                                              command=self.__changeExposure)
+        self.numExposure.setScale(0.5)
+        self.numExposure.setTransparency(True)
+        self.numExposureText = OnscreenText(pos=(self.buttonbase_xcoord + 0.1, self.buttonbase_ycoord - self.textRowHeight * 2.2), scale=0.05, text="Exposure Amount = {}".format(self.blur), style=5, mayChange=True)
+
+
+    def printValue(self):
+        print(self.numSamples['value'])
+
+    def __changeCartoon(self):
+        self.filter.delCartoonInk()
+        s = self.cSep['value']
+        self.cartoonSep = s
+        r = self.cRed['value']
+        self.cartoonR = r
+        b = self.cBlue['value']
+        self.cartoonB = b
+        g = self.cGreen['value']
+        self.cartoonG = g
+        self.filter.setCartoonInk(s, (r, g, b, 1)) # a doesn't change
+
+    def __changeBlur(self):
+        self.filter.delBlurSharpen()
+        b = self.numBlur['value']
+        self.blur = b
+        self.numBlurText.setText("Blur amount = {}".format(b))
+        self.filter.setBlurSharpen(b)
+
+    def __changeAOValue(self):
+        if self.numSamples is None:
+            print("NONE")
+            return
+        self.filter.delAmbientOcclusion()
+
+        s = self.numSamples['value']
+        self.samples = s
+        self.numSamplesText.setText("AO Sample Count = {}".format(s))
+        if(s == 0):
+            return
+
+        r = self.numRadius['value']
+        self.radius = r
+        self.numRadiusText.setText("AO Radius = {}".format(r))
+        if(r == 0):
+            return
+
+        a = self.numAmount['value']
+        self.amount = a
+        self.numAmountText.setText("AO Amount = {}".format(a))
+        if(a == 0):
+            return
+
+        st = self.numStrength['value']
+        self.strength = st
+        self.numStrengthText.setText("AO Strength = {}".format(st))
+        if(st == 0):
+            return
+
+        self.filter.setAmbientOcclusion(numsamples=s, radius=r, amount=a, strength=st)
+        print("sample count: {} | radius count: {} | amount count: {} | strength count: {}".format(self.samples, self.radius, self.amount, self.strength))
+
+    # WARNING: Won't work with relatively older Panda versions because this is new
+    def __changeExposure(self):
+        self.filter.delExposureAdjust()
+        e = self.numExposure['value']
+        self.exposure = e
+        self.numExposureText.setText("Exposure amount = {}".format(e))
+        self.filter.setExposureAdjust(e)
+
+    def __toggleInverted(self):
+        if not self.invertedEnabled:
+            self.filter.setInverted()
+            self.invertedEnabled = True
+        else:
+            self.filter.delInverted()
+            self.invertedEnabled = False
+
+    def __toggleHDR(self):
+        if not self.HDREnabled:
+            self.filter.setHighDynamicRange()
+            self.HDREnabled = True
+        else:
+            self.filter.delHighDynamicRange()
+            self.HDREnabled = False
+
+    def __toggleSRGB(self):
+        if not self.sRGBEnabled:
+            self.filter.setSrgbEncode(True)
+            self.sRGBEnabled = True
+        else:
+            self.filter.delSrgbEncode()
+            self.sRGBEnabled = False
+
+    def __toggleHalfPixelShift(self):
+        if not self.halfPixelShiftEnabled:
+            self.filter.setHalfPixelShift()
+            self.halfPixelShiftEnabled = True
+        else:
+            self.filter.delHalfPixelShift()
+            self.halfPixelShiftEnabled = False
+
+    def __toggleViewGlow(self):
+        if not self.viewGlowEnabled:
+            self.filter.setViewGlow()
+            self.viewGlowEnabled = True
+        else:
+            self.filter.delViewGlow()
+            self.viewGlowEnabled = False
+
+    def getValue(self, item, item_MAX):
+        return item / item_MAX
+
+    # https://discourse.panda3d.org/t/how-to-open-a-new-window/23929/4
+    def newWindow(self):
+        self.wp = WindowProperties()
+        self.wp.setSize(700, 500)
+        win2mouseWatcher = MouseWatcher()
+        ar = 1.33
+        self.win2 = base.openWindow(props=self.wp, aspectRatio=ar)
+        self.window2render2d = NodePath('window2render2d')
+        self.window2render2d.setDepthWrite(0)
+        self.window2camera2d = base.makeCamera2dp(self.win2)
+        self.window2camera2d.reparentTo(self.window2render2d)
+
+        self.window2aspect2d = self.window2render2d.attachNewNode(PGTop('window2aspect2d'))
+        self.window2aspect2d.setScale(1.0 / ar, 1.0, 1.0)
+        self.window2aspect2d.node().setMouseWatcher(win2mouseWatcher)
+
+        self.loadBlurGUI()
+
+
+######
+
+"""
+from toontown.shader import ToontownShaderManager
+tsm= ToontownShaderManager.ToontownShaderManager()
+"""
---
+++ scripts/pandashaders/demo.py
@@ -0,0 +1,204 @@
+from direct.showbase.ShowBase import ShowBase
+from pathlib import Path
+from tkinter.filedialog import askopenfilename
+from panda3d.core import Filename
+from panda3d.core import GraphicsOutput
+from panda3d.core import CullFaceAttrib
+from panda3d.core import NodePath
+from direct.gui.DirectGui import *
+import sys, os
+import ToontownShaderManager
+from panda3d.core import loadPrcFileData
+loadPrcFileData('', 'model-path $RESOURCE_DIR')
+loadPrcFileData('', 'default-antialias-enable 1')
+loadPrcFileData('', 'framebuffer-multisample 1')
+#loadPrcFileData('', 'want-pstats #t')
+# We need to import the tkinter library to
+# disable the tk window that pops up.
+# We use tk for the file path selector.
+import tkinter as tk
+root = tk.Tk()
+root.withdraw()
+
+
+class generate(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.base = ShowBase
+        self.model = None
+        self.lastModelPath = ""
+        self.collisionsEnabled = False
+        self.collisionsExist = False
+        self.SGMeterToggled = False
+        self.tightboundsToggled = False
+        self.boundsToggled = False
+        self.frontfaceCullingEnabled = False
+        self.vertexPaintingToggled = False
+        self.occludersEnabled = False
+        self.occluderExist = False
+        self.modelNodesEnabled = False
+        self.modelNodesExist = False
+        self.XYZAxis = loader.loadModel("models/misc/xyzAxis.bam")
+        self.showNode = NodePath()
+        self.tsm= ToontownShaderManager.ToontownShaderManager(base.aspect2d)
+
+        self.xyz = None
+
+
+
+        self.accept('o', base.oobe)
+
+        self.loadGUI()
+
+        self.accept('q', self.loadLastModel)
+        self.accept('a', render.analyze)
+        self.accept('r', self.resetCam)
+        self.accept('c', self.clearScene)
+        self.accept('s', base.screenshot)
+
+
+        self.accept('1', base.toggleWireframe)
+        self.accept('2', base.toggleTexture)
+        self.accept('3', self.toggleVertexPainting)
+
+
+
+
+
+    def loadGUI(self):
+        self.topButton = DirectButton(text=("Load model"),
+                 scale=0.05, pos=(0, 0, -0.90), parent=base.aspect2d, command=self.loadFile)
+
+    def browseModel(self):
+        path = Path(askopenfilename(filetypes = (
+            ("Image Files", "*.egg;*.bam"),
+            ("EGG", "*.egg"),
+            ("BAM", "*.bam"))))
+        return path
+
+    def loadFile(self):
+        filename = self.browseModel()
+        if str(filename) == ".":
+            return
+        try:
+            self.loadModel(filename)
+        except:
+            print(str(filename) + " could not be loaded!")
+
+    def loadModel(self, file: str):
+        self.clearScene()
+        self.lastModelPath = file
+        self.model = loader.loadModel(file)
+        self.model.reparentTo(render)
+        print("========================================")
+        print("Loaded {}".format(file))
+
+    def loadLastModel(self):
+        self.loadModel(self.lastModelPath)
+
+    def resetCam(self):
+        base.oobe()
+        base.cam.setPosHpr(0, 0, 0, 0, 0, 0)
+        base.oobe()
+
+    def clearScene(self):
+        if self.model is not None:
+            self.model.removeNode()
+            self.model = None
+        if self.xyz is not None:
+            self.xyz.removeNode()
+            self.xyz = None
+
+    def toggleVertexPainting(self):
+        if not self.vertexPaintingToggled:
+            render.setColor(1, 1, 1)
+            self.vertexPaintingToggled = True
+        else:
+            render.clearColor()
+            self.vertexPaintingToggled = False
+
+    def toggleFrontfaceCulling(self):
+        if not self.frontfaceCullingEnabled:
+            render.setAttrib(CullFaceAttrib.make(CullFaceAttrib.MCullCounterClockwise))
+            self.frontfaceCullingEnabled = True
+        else:
+            render.clearAttrib(CullFaceAttrib)
+            self.frontfaceCullingEnabled = False
+
+
+    def toggleTightBounds(self):
+        if not self.tightboundsToggled or self.boundsToggled:
+            for node in render.getChild(0).getChildren():
+                node.showTightBounds()
+            self.tightboundsToggled = True
+            self.boundsToggled = False
+        else:
+            for node in render.getChild(0).getChildren():
+                node.hideBounds()
+            self.tightboundsToggled = self.boundsToggled = False
+
+    def toggleCollisionNodes(self):
+        if not self.collisionsEnabled:
+            for node in render.findAllMatches('**/CollisionNode'):
+                self.collisionsExist = True
+                self.collisionsEnabled = True
+                node.show()
+                print("Found collision node: {}".format(node))
+                print("Collisions enabled")
+            if not self.collisionsExist:
+                print("No collision nodes found.")
+        else:
+            render.findAllMatches('**/+CollisionNode').hide()
+            self.collisionsEnabled = False
+            print("Collisions disabled")
+
+    def toggleOccluderNodes(self):
+        if not self.occludersEnabled:
+            for node in render.findAllMatches('**/+OccluderNode'):
+                self.occluderExist = True
+                self.occludersEnabled = True
+                node.show() # i haven't seen an occluder node before so idk if they have any geom
+                print("Found occluder node: {}".format(node))
+                print("Occluders enabled")
+            if not self.occluderExist:
+                print("No occluder nodes found.")
+        else:
+            render.findAllMatches('**/+OccluderNode').hide()
+            self.occludersEnabled = False
+            print("Occluders disabled")
+
+    # todo: figure out the deal going on with the cycle assertion error :|
+    def toggleModelNodes(self):
+        if not self.modelNodesEnabled:
+            for node in render.findAllMatches('**/+ModelNode'):
+                self.modelNodesExist = True
+                self.modelNodesEnabled = True
+                self.xyz = self.XYZAxis.instanceTo(node) #copyTo bad
+                self.xyz.setPos(node.getPos())
+                self.xyz.setHpr(node.getHpr())
+                node.show() # most of the time there's no geometry.
+                print("Found ModelNode: {} at pos:{} hpr:{}".format(node, node.getPos(), node.getHpr()))
+                print("ModelNodes visible")
+            if not self.modelNodesExist:
+                print("No model nodes found.")
+        else:
+            render.findAllMatches('**/ModelNode').hide()
+            for node in self.XYZAxis.getChildren():
+                node.removeNode()
+            self.xyz.removeNode()
+            self.xyz = None
+            self.modelNodesEnabled = False
+            print("Model nodes disabled")
+
+    def listNodes(self):
+        if self.model is None:
+            return # can't return nodes of something that doesn't exist
+        print()
+        print("-x-x-x-x-x-x-x-x-x-x-x-x-")
+        self.model.ls()
+        print("-x-x-x-x-x-x-x-x-x-x-x-x-")
+        print()
+
+
+app = generate()
+app.run()
