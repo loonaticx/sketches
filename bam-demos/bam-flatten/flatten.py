b65e959e17d1c1dd80b81cbea8ded8b7a1d1f4e3
loonaticx <l.oony@aol.com>
2021-08-11 17:34:51 -0500

    Add flatten script for feasibility

--- bam-demos/bam-flatten/flatten.py
+++ bam-demos/bam-flatten/flatten.py
@@ -1,21 +1,29 @@
 from panda3d.core import *
+import sys
 
-input = "input.egg"
+input = sys.argv[1]
+mode = sys.argv[2]
+mode = int(mode)
+
+newfile = str(input).replace(".egg", ".bam")
 
 loader = Loader.getGlobalPtr()
 node = loader.loadSync(input)
 
-bam_normal = NodePath(node)
-bam_normal.writeBamFile("output_normal.bam")
-
-bam_light = NodePath(node)
-bam_light.flattenLight()
-bam_light.writeBamFile("output_light.bam")
-
-bam_med = NodePath(node)
-bam_med.flattenMedium()
-bam_med.writeBamFile("output_medium.bam")
-
-bam_strong = NodePath(node)
-bam_strong.flattenStrong()
-bam_strong.writeBamFile("output_strong.bam")
+if mode == 0:
+    bam_normal = NodePath(node)
+    bam_normal.writeBamFile(newfile)
+elif mode == 3:
+    bam_light = NodePath(node)
+    bam_light.flattenLight()
+    bam_light.writeBamFile(newfile)
+elif mode == 2:
+    bam_med = NodePath(node)
+    bam_med.flattenMedium()
+    bam_med.writeBamFile(newfile)
+elif mode == 1:
+    bam_strong = NodePath(node)
+    bam_strong.flattenStrong()
+    bam_strong.writeBamFile(newfile)
+else:
+    print("Invalid mode")
