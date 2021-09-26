40ae303a0384e780246c3c2433cd98e51873a86c
loonaticx <l.oony@aol.com>
2021-09-26 01:11:50 -0500

    debug: add python 2.7+3.7 to fixregistry and remove pause on clearmodelcache

--- debug/clearmodelcache.bat
+++ debug/clearmodelcache.bat
@@ -5,4 +5,3 @@ rem This is useful when you want to see error info (eg "Texture cannot be found 
 rem This can also potentially fix issues with models/textures not updating after modifying them.
 
 for /d %%G in ("C:\Users\%username%\AppData\Local\Panda3D-*") do echo Removing %%G & rd /s /q %%G
-pause 
