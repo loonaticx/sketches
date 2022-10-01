b66c9c4c3d0ece5fd34460f63e36dfb048f9259e
Erica M. ("Loonatic") <l.oony@aol.com>
2022-10-01 01:08:32 -0500

    Update clearmodelcache.bat

--- debug/clearmodelcache.bat
+++ debug/clearmodelcache.bat
@@ -4,4 +4,4 @@ rem This will clear Panda3D's model cache.
 rem This is useful when you want to see error info (eg "Texture cannot be found ....") when opening up bam/egg files again.
 rem This can also potentially fix issues with models/textures not updating after modifying them.
 
-for /d %%G in ("C:\Users\%username%\AppData\Local\Panda3D-*") do echo Removing %%G & rd /s /q %%G
+for /d %%G in ("C:\Users\%username%\AppData\Local\Panda3D-*") do echo Removing %%G & rd /s /q %%G & mkdir %%G
